5df6b63cd176aabcb50bbe494d0ee517ccaf594c
tihana <tihana.gereci@gmail.com>
2020-04-18 17:42:56 +0100

    add gradle, solution main and test classes, update .gitignore

---
+++ src/main/java/org/tihana/Solution.java
@@ -0,0 +1,13 @@
+package org.tihana;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Solution {
+
+    private static final Logger logger = LoggerFactory.getLogger(Solution.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello world");
+    }
+}
---
+++ src/test/java/org/tihana/SolutionTest.java
@@ -0,0 +1,13 @@
+package org.tihana;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+}
